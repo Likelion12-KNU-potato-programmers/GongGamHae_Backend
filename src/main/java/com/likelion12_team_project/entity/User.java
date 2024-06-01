4457e7824e679f9e9e8b51e33090d60dc59a200d
GamJaDo <kim0615157@gmail.com>
2024-06-02 05:16:03 +0900

    userid -> userAccount
    userid는 primary의 id와 이름이 유사하여 정확한 구분을 위해 userid를 userAccount로 변경

--- src/main/java/com/likelion12_team_project/entity/User.java
+++ src/main/java/com/likelion12_team_project/entity/User.java
@@ -16,7 +16,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String userid;
+    private String userAccount;
     private String password;
     private String nickname;
     private String profileImageUrl;
