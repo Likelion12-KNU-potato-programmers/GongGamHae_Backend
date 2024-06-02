c726b36e823fb9c4d028592a8cd57be818b03743
GamJaDo <kim0615157@gmail.com>
2024-06-02 23:19:20 +0900

    최근에 작성된 3개의 포스트를 가져오는 쿼리 메소드 작성

--- src/main/java/com/likelion12_team_project/repository/GonggamPostRepository.java
+++ src/main/java/com/likelion12_team_project/repository/GonggamPostRepository.java
@@ -2,12 +2,17 @@ package com.likelion12_team_project.repository;
 
 import com.likelion12_team_project.entity.GonggamPost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface GonggamPostRepository extends JpaRepository<GonggamPost, Long>{
-    List<GonggamPost> findByLikesGreaterThan(int likes);
+	List<GonggamPost> findByLikesGreaterThan(int likes);
     List<GonggamPost> findByUserId(Long userId);
+
+    // 최근에 작성된 3개의 포스트를 가져오는 쿼리 메소드
+    List<GonggamPost> findTop3ByOrderByCreatedAtDesc();
+
 }
